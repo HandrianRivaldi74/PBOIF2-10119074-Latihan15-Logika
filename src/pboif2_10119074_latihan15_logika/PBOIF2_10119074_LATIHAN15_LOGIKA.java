94de76683b0ea061d60105718670af5e093e0b44
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 17:10:59 +0700

    LATIHAN15

---
+++ src/pboif2_10119074_latihan15_logika/PBOIF2_10119074_LATIHAN15_LOGIKA.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan15_logika;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * deskrifsi : Logika 2
+ */
+public class PBOIF2_10119074_LATIHAN15_LOGIKA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
